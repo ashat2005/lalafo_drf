ae533b29ae5e2d09f284e1c96e5941de451f5994
ashat2005 <ashatkydyrov61@gmail.com>
2023-10-08 15:41:03 -0400

    add user model and endpoint

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'apps.posts',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
