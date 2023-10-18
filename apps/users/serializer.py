c3c09e45c9b1a7e880b34890ecfcc8a715d1498f
ashat2005 <ashatkydyrov61@gmail.com>
2023-10-18 09:47:59 -0400

    urok 3

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from apps.users.models import User
-
+from apps.posts.serializer import PostSerializer
+ 
 class UserSerializer(serializers.ModelSerializer):
+    user_posts = PostSerializer(many=True, read_only=True)
     class Meta:
         model = User
         fields = ('id','username','first_name','last_name',
-                  'email','date_joined','phone_number','profile_image')
+                  'email','date_joined','phone_number','profile_image','user_posts')
